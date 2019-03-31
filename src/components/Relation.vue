c44f734adc1ca547f594e5d222edebe990f09476
landchootl <ludovic.landschoot.partner@decathlon.com>
2019-03-31 23:56:15 +0200

    refactor (relation): change text relation

--- src/components/Relation.vue
+++ src/components/Relation.vue
@@ -2,10 +2,8 @@
     <v-parallax :src="require('@/assets/cover2.jpg')" height="380">
         <v-layout column align-center justify-center>
             <div class="headline white--text mb-3 text-xs-center" style="font-size: 30px;">Vous avez un besoin ?</div>
-            <em style="font-size: 19px;">Venez discuter avec nous !</em>
-            <v-btn class="blue lighten-2 mt-5" dark large href="/pre-made-themes">
-                Demander une mise en relation
-            </v-btn>
+            <em style="font-size: 19px;">N'hésitez pas à nous en faire part !</em>
+            <v-btn class="blue lighten-2 mt-5" dark large href="/pre-made-themes">Demander un devis</v-btn>
         </v-layout>
     </v-parallax>
 </template>
