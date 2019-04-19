311384fea77af3621813e6f162fc1d742a9310b6
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-04-19 11:31:22 +0200

    refactor(catalog): static version of the catalog bis

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,27 +1,32 @@
 <template>
-    <v-layout column wrap class="my-5" align-center>
-        <v-flex xs12 sm4 class="my-3">
+    <v-layout column wrap class="my-1">
+        <v-flex id="id_title" xs12 sm4 class="my-3">
             <div class="text-xs-center">
-                <h2 class="headline">Nos formations !</h2>
+                <h2 class="headline">FORMATIONS & AUDIT</h2>
                 <span class="subheading">
                     Un savoir-faire diversifié
-              </span>
+                </span>
             </div>
         </v-flex>
         <v-flex xs12>
-            <v-container grid-list-xl>
-                <v-layout row wrap align-center>
-                    <v-flex xs12 md3 v-for="(domain, index) in domains" :key="index">
+            <v-container>
+                <v-layout align-top>
+                    <v-flex id="column" xs12 md3 v-for="(domain, index) in domains" :key="index">
                         <v-card class="elevation-0 transparent">
                             <v-card-text class="text-xs-center">
                                 <v-icon x-large class="blue--text text--lighten-2">{{domain.icon}}</v-icon>
                             </v-card-text>
                             <v-card-title primary-title class="layout justify-center">
-                                <div class="headline">{{domain.name}}</div>
+                                <div id="id_domain" class="headline">{{domain.title}}</div>
                             </v-card-title>
+                            <hr>
                             <v-card-text>
                                 <ul>
-                                    <li v-for="(skill, index) in domain.skills" :key="index">{{skill.name}}</li>
+                                    <li v-for="(subdomain, index) in domain.subdomains" :key="index"> <div id="id_subdomain">{{subdomain.title}}</div>
+                                        <ul>
+                                            <li id="list_skills" v-for="(skill, index) in subdomain.skills" :key="index">{{skill.title}}</li>
+                                        </ul>
+                                    </li>
                                 </ul>
                             </v-card-text>
                         </v-card>
@@ -52,5 +57,45 @@
 </script>
 
 <style scoped>
+
+    #column:nth-child(odd){
+        background: #F0F0F0;
+    }
+
+    #id_domain {
+        color: #15BFAB;
+    }
+
+    #list_skills {
+        font-weight: normal;
+    }
+
+    #list_skills::before {
+        content: "•";
+        color: #15BFAB;
+        display: inline-block;
+        width: 1em;
+        margin-left: -2em;
+    }
+
+    #id_subdomain {
+        font-weight: bold;
+        padding-top: 20px;
+    }
+
+    hr {
+        display: block;
+        color: black;
+        width: 30%;
+        margin-left: auto;
+        margin-right: auto;
+        height: 3px;
+        border-style: solid;
+        border-width: 1.5px;
+    }
+
+    ul {
+        list-style: none;
+    }
 
 </style>
