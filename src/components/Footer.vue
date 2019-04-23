d8a42f3a3b09054f2c7ea92992baf8ae0a44455b
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-04-23 10:45:07 +0200

    refactor(footer): delete footer vuetify

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,7 @@
 <template>
     <v-footer class="blue darken-2">
         <v-layout row wrap align-center>
-            <!--
-            <v-flex xs12>
-                <div class="white--text ml-3">
-                    Made with
-                    <v-icon class="red--text">favorite</v-icon>
-                    by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
-                </div>
-            </v-flex>
-            -->
+
         </v-layout>
     </v-footer>
 </template>
