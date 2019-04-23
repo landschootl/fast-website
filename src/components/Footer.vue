76b3ea3ed957600863a3ead8b9d21e9ffa48f846
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-04-23 10:11:45 +0200

    refactor(footer): delete made with love by vuetify

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
     <v-footer class="blue darken-2">
         <v-layout row wrap align-center>
+            <!--
             <v-flex xs12>
                 <div class="white--text ml-3">
                     Made with
@@ -8,6 +9,7 @@
                     by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
                 </div>
             </v-flex>
+            -->
         </v-layout>
     </v-footer>
 </template>
