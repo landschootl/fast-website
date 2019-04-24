400adcbcf694b4c85eab127c3200d227edddaa80
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-04-24 16:10:21 +0200

    refactor(catalog): restoring the json

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -26,7 +26,6 @@
                                     <li v-for="(subdomain, index) in domain.subdomains" :key="index"> <div id="id_subdomain">{{subdomain.title}}</div>
                                         <ul>
                                             <li id="list_skills" v-for="(skill, index) in subdomain.skills" :key="index">
-                                                <input id="showCheckbox" type="checkbox" name="{{skill.title}}" value="{{skill.title}}">
                                                 {{skill.title}}
                                             </li>
                                         </ul>
@@ -54,6 +53,7 @@
             //     throw new Error(error.response.data);
             // });
 
+            this.domains = DomainService.getAll();
             /**
             DomainService.getAll().then((response) => {
                 console.log((response))
