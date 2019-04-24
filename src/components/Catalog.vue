709bd0de0bf7ac0e23b52e75eca4f2c9c77487ef
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-04-24 16:02:42 +0200

    refactor(domainService): put in commentary the getAll for the fetch

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -55,7 +55,7 @@
             // }).catch((error) => {
             //     throw new Error(error.response.data);
             // });
-
+            
             this.domains = DomainService.getAll();
             /**
             DomainService.getAll().then((response) => {
