69f468e1df67d7370a460d6da453a58956525812
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-04-24 15:54:22 +0200

    refactor(RelationForm): deleting ShowCheckBox in css

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -26,7 +26,6 @@
                                 <ul>
                                     <li v-for="(subdomain, index) in domain.subdomains" :key="index"> <div id="id_subdomain">{{subdomain.title}}</div>
                                         <ul>
-
                                             <li id="list_skills" v-for="(skill, index) in subdomain.skills" :key="index">
                                                 <input type="checkbox" id="checkbox" v-if="showCheckbox" v-model="registration.skills" v-bind:value="skill">
                                                 {{skill.title}}
