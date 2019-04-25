c3ce880bdac2ff72197feaec3ffefebde3c1a04c
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-04-25 14:58:42 +0200

    refactor(cover): resize the cover

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -1,7 +1,7 @@
 <template>
     <v-parallax
             :src="require('@/assets/cover.jpg')"
-            height="600">
+            height="400">
         <v-layout column align-center justify-center class="white--text">
             <img src="../assets/logofast.png" alt="Vuetify.js" height="200">
             <h1 class="white--text mb-2 display-1 text-xs-center">{{title}}</h1>
