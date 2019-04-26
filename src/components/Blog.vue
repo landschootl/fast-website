776394d46b4673204b50f70b60ba1e34f248b466
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-04-26 09:58:32 +0200

    refactor(blog): adding cover

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -2,16 +2,20 @@
   <v-app light>
     <v-content>
       <section>
-        BLOG
+        <Cover_Blog/>
       </section>
     </v-content>
   </v-app>
 </template>
 
 <script>
+
+    import Cover_Blog from "./Cover_Blog";
+
     export default {
         name: "Home",
         components: {
+          Cover_Blog
         },
         data: () => ({
 
---
+++ src/components/Cover_Blog.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-parallax
+            :src="require('@/assets/cover_blog.jpg')"
+            height="500">
+        <v-layout column align-center justify-center class="white--text">
+            <img src="../assets/logofast.png" alt="Vuetify.js" height="200">
+            <h1 class="white--text mb-2 display-1 text-xs-center">{{title}}</h1>
+            <div class="subheading mb-3 text-xs-center">
+                <span>{{description}}</span><br>
+                <span class="cyan--text">{{sub_description}}</span>
+            </div>
+        </v-layout>
+    </v-parallax>
+</template>
+
+<script>
+    export default {
+        name: "Cover_Blog",
+        data: () => ({
+            title: "BLOG"
+        })
+    }
+</script>
+
+<style scoped>
+
+</style>
