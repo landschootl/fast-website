c6ab31e08a6638a204e0aa5c6ec67d96971aaaae
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-04-30 17:12:10 +0200

    refactor(font): addinf font family

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import Axios from 'axios';
 
+import "./stylus/main.styl";
+
 import './plugins/vuetify'
 import './plugins/vue-router'
 
