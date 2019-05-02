fbebef76e88c6d6c0caa5b90403370a50683178a
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-05-02 11:15:40 +0200

    refactor(Testimony): stating json for testimony

---
+++ src/services/testimony.service.js
@@ -0,0 +1,12 @@
+export default {
+    getAllTestimony() {
+
+        return [{"nom":"Nicolas Dupont","image":"","description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt, leo ac rutrum mollis, augue libero pharetra nibh, nec blandit velit nisl commodo turpis. Aenean non laoreet ipsum"},{"nom":"Amanda Selosse","image":"","description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt, leo ac rutrum mollis, augue libero pharetra nibh, nec blandit velit nisl commodo turpis. Aenean non laoreet ipsum"},
+            {"nom":"Valentin Tarantino","image":"","description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt, leo ac rutrum mollis, augue libero pharetra nibh, nec blandit velit nisl commodo turpis. Aenean non laoreet ipsum"},{"nom":"Isabelle leroy","image":"","description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt, leo ac rutrum mollis, augue libero pharetra nibh, nec blandit velit nisl commodo turpis. Aenean non laoreet ipsum"}];
+        /*return fetch('http://localhost:8080/api/v1/domains')
+            .then((response) => response.json())
+            .then((data) => {
+                return data;
+            })*/
+    }
+};
