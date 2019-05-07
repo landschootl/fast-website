47a97b4bb72a8b3654b9b3f0cfdad58ef91d3981
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-05-07 10:41:27 +0200

    refactor(testimony): restoring carousel

--- src/components/Testimony.vue
+++ src/components/Testimony.vue
@@ -1,54 +1,50 @@
 <template>
-    <v-carousel light hide-delimiters>
-        <v-carousel-item>
-            <img :src="require('../assets/testimony_background.jpg')" />
-        </v-carousel-item>
-        <v-carousel-item>
-            <img :src="require('../assets/testimony_background.jpg')" />
-            <v-carousel  class="cara" dark hide-delimiters>
+    <v-carousel dark hide-delimiters>
                 <v-carousel-item height="100%" v-for="(testimony, index) in testimonies" :key="index">
                     <v-responsive dark height="100%" >
                         <v-img id="id_background" :src="require('@/assets/testimony_background.jpg')"/>
-                        <v-layout align-center column justify-center class="header-text">
-                            <v-avatar :tile="false" :size="100">
+                        <v-layout align-center column justify-center>
+                            <v-avatar :tile="false" :size="100" class="header-image">
                                 <v-img :src="testimony.image"/>
                             </v-avatar>
-                            <v-flex  xs12 class="text-xs-center">
+                            <v-flex  xs12 class="text-xs-center header-text">
                                 <blockquote class="blockquote near-white-dav" id="id_testimony_name">{{testimony.nom}}</blockquote>
-                                <blockquote class="blockquote near-white-dav" id="id_testimony_description">{{testimony.description}}</blockquote>
+                                <blockquote class="blockquote near-white-dav">{{testimony.description}}</blockquote>
                             </v-flex>
                         </v-layout>
                     </v-responsive>
                 </v-carousel-item>
             </v-carousel>
-        </v-carousel-item>
-    </v-carousel>
 </template>
 
 <script>
     import TestimonyService from '../services/testimony.service';
     export default {
         name: "Testimony",
-
         created() {
             /*TestimonyService.getAllTestimony().then((response) => {
                 this.domains = response;
             })*/
             this.testimonies = TestimonyService.getAllTestimony();
         },
-
         data: () => ({
             testimonies: []
         })
     }
-
 </script>
 
 <style scoped>
-
     .header-text {
         position: absolute;
-        top: 20%;
+        top: 40%;
+        left: auto;
+        right: auto;
+        width: 96.66666666666666%;
+    }
+
+    .header-image {
+        position: absolute;
+        top: 15%;
         left: auto;
         right: auto;
         width: 96.66666666666666%;
@@ -57,5 +53,4 @@
     #id_testimony_name {
         font-size: 200%;
     }
-
 </style>
