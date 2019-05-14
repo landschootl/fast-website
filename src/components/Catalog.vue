0a877f094946b4646fa5b25c394e7a6d250238dd
MarjorieStaquet <marjorie.staquet@davidson.fr>
2019-05-14 13:50:15 +0200

    refactor(catalog): changing negative margin

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,45 +1,49 @@
 <template>
     <v-layout column wrap class="my-1 layout_global">
-        <v-flex v-if="!showCheckbox" xs12 sm4 class="my-3 flex_title">
-            <div class="text-xs-center">
-                <h2 class="headline ATCArquette-Medium">FORMATIONS & AUDITS</h2>
-                <span class="subheading ATCArquette-Light">
-                    Un savoir-faire diversifié
-                </span>
-            </div>
-        </v-flex>
         <v-flex xs12>
             <v-container id="id_container" grid-list-xl>
-                <v-layout  row wrap justify-center align-top>
-                    <v-flex id="column" xs12 md3 v-for="(domain, index) in domains" :key="index">
-                        <v-card class="elevation-0 transparent">
-                            <v-card-text v-if="!showCheckbox" class="text-xs-center">
-                                <v-icon class="turquoise-dav" x-large>{{domain.icon}}</v-icon>
-                            </v-card-text>
-                            <v-card-title primary-title class="layout justify-center">
-                                <div class="headline turquoise-dav ATCArquette-Medium">{{domain.title}}</div>
-                            </v-card-title>
-                            <hr width="30%" height="3px" class="near-black-dav"/>
-                            <v-card-text>
-                                <form>
-                                <ul>
-                                    <li v-for="(subdomain, index) in domain.subdomains" :key="index"> <div class="ATCArquette-Bold" id="id_subdomain">{{subdomain.title}}</div>
-                                        <ul>
-                                            <li class="ATCArquette-Medium near-black-dav" id="list_skills" v-for="(skill, index) in subdomain.skills" :key="index">
-                                                <input type="checkbox" id="checkbox" v-if="showCheckbox" v-model="registration.skills" v-bind:value="skill">
-                                                {{skill.title}}
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                                </form>
-                            </v-card-text>
-                        </v-card>
+                <v-layout v-if="!showCheckbox" align-center justify-center column fill-height>
+                    <v-flex xs12 sm4>
+                        <div class="text-xs-center">
+                            <h2 class="headline ATCArquette-Medium turquoise-dav">FORMATIONS & AUDITS</h2>
+                            <span class="subheading ATCArquette-Light">
+                                Un savoir-faire diversifié
+                             </span>
+                        </div>
                     </v-flex>
                 </v-layout>
-                <v-btn class="btn_continue" v-if="showCheckbox" color="primary" @click="validate">Continue</v-btn>
+                    <v-layout  row wrap justify-center align-top>
+                        <v-flex id="column" xs12 md3 v-for="(domain, index) in domains" :key="index">
+                            <v-card class="elevation-0 transparent">
+                                <v-card-text v-if="!showCheckbox" class="text-xs-center">
+                                    <v-icon class="turquoise-dav" x-large>{{domain.icon}}</v-icon>
+                                </v-card-text>
+                                <v-card-title v-if="!showCheckbox" primary-title class="layout justify-center">
+                                    <div class="headline turquoise-dav ATCArquette-Medium">{{domain.title}}</div>
+                                </v-card-title>
+                                <hr v-if="!showCheckbox" width="30%" height="3px" class="near-black-dav"/>
+                                <v-card-text>
+                                    <form>
+                                        <ul>
+                                            <li v-for="(subdomain, index) in domain.subdomains" :key="index"> <div class="ATCArquette-Bold" id="id_subdomain">{{subdomain.title}}</div>
+                                                <ul>
+                                                    <li class="ATCArquette-Medium near-black-dav bef-turquoise-dav" id="list_skills" v-for="(skill, index) in subdomain.skills" :key="index">
+                                                        <input type="checkbox" id="checkbox" v-if="showCheckbox" v-model="registration.skills" v-bind:value="skill">
+                                                        {{skill.title}}
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </form>
+                                </v-card-text>
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
             </v-container>
         </v-flex>
+        <v-layout align-end justify-center column fill-height>
+             <v-btn v-if="showCheckbox" color="primary" @click="validate">Continue</v-btn>
+        </v-layout>
     </v-layout>
 </template>
 
@@ -81,11 +85,6 @@
 </script>
 
 <style scoped lang="scss">
-    #column {
-        margin-top: -6%;
-        padding-top: 7%;
-        padding-bottom: 5%;
-    }
 
     #column:nth-child(odd) {
         background-color: rgba(238, 240, 239, 0.5);
@@ -97,10 +96,6 @@
         overflow: hidden;
     }
 
-    .flex_title {
-        z-index: 3;
-    }
-
     #id_container {
         padding: 0px !important;
         max-width: none !important;
@@ -108,7 +103,6 @@
 
     #list_skills::before {
         content: "•";
-        color: #33CCCC;
         display: inline-block;
         width: 1em;
     }
